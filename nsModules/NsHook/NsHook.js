652fdbf5777c4f243641296eb1a24a41f677a73c
gitkyo <p.delaunay1@gmail.com>
2015-05-27 13:35:12 +0200

    [#616] - Amélioration BO

--- nsModules/NsHook/NsHook.js
+++ nsModules/NsHook/NsHook.js
@@ -1,80 +1,40 @@
 (function ($) {
   Drupal.behaviors.NsHook = {
     attach: function (context, settings) {
-      
 
-	    //All multiple upload images
-	   //  $('.field-type-image table.sticky-enabled tr', context).change(function () {
-	        
-	   //    	var tr = $(this);
+    	//Si on est dans l'edition ou l'ajout d'une page balade
+    	if($('body.page-node-add-balade').length > 0 || $('body.node-type-balade').length > 0){
 
-	   //    	tr.find('.form-type-textfield').each(function(index){
+	   					
+			//Dès qu'il y a une requette ajax sur l'edition de balades  
+		    $('.node-form.node-balade-form').ajaxComplete(function(event, xhr, settings) {
+			    
+			    
+			    //Photo résumé
+			    $('#edit-field-photo-resume').find('.form-type-textfield').each(function(index){
 
-	      		
-	   //    		console.log( $(this).find('.description').text() );
-	      		
-				// // Ce texte sera utilisé par les lecteurs d'écran, les moteurs de recherche, ou lorsque l'image ne peut être chargée.
-				// // NsHook.js?nnivjs:13 Le titre apparait sous la forme d'une infobulle lorsque l'utilisateur survole l'image avec sa souris.
+			    	if(index == 0) $(this).find('.description').html("Cette description est à destination des malvoyants et moteurs de recherches. <br/> Si possible ré-utiliser le <i>titre de la balade</i> ainsi que les mots clefs : <i>nature, balade, rando, radonnées, faune, flore</i>");
+			    	if(index == 1) $(this).find('.description').html("Si des crédits sont associés à la photo, veuillez l'ajouter entre parenthèses. <br/> Pas de majuscule ou accent <br/> Préferer le séparateur '-' entre les expressions <br/> Si possible ré-utiliser le <i>titre de la balade</i> ainsi que les mots clefs : <i>nature, balade, rando, radonnées, faune, flore</i>");
 
-	   //    	});
-	     	
+			    });
 
-	   //  });
-		
-		$('div.field-name-field-esp-ces > div.form-item-field-esp-ces-und > label').after('<br/><span class="textAide">Voici la liste des espèces présente dans le site ecobalade.fr, il vous suffit de cocher les espèces présente dans votre balade.</span>');
+			    //Photos 
+			    $('#edit-field-image table.sticky-table tr').each(function(index){
 
-		//Dès qu'il y a une requette ajax sur l'edition de balades  
-	    $('.node-form.node-balade-form').ajaxComplete(function(event, xhr, settings) {
-		    	
-		    
-		    //console.log($(event));
+		    		$(this).find('.form-type-textfield').each(function(i){
+		    		
+		    			if(i == 0) $(this).find('.description').html("Cette description est à destination des malvoyants et moteurs de recherches. <br/> Si possible ré-utiliser le <i>titre de la balade</i> ainsi que les mots clefs : <i>nature, balade, rando, radonnées, faune, flore</i>");
+		    			if(i == 1) $(this).find('.description').html("Si des crédits sont associés à la photo, veuillez l'ajouter entre parenthèses. <br/> Pas de majuscule ou accent <br/> Préferer le séparateur '-' entre les expressions <br/> Si possible ré-utiliser le <i>titre de la balade</i> ainsi que les mots clefs : <i>nature, balade, rando, radonnées, faune, flore</i>");
+		    		
+		    		});	    		
 
-		    //Photo résumé
-		    $('#edit-field-photo-resume').find('.form-type-textfield').each(function(index){
-
-		    	if(index == 0) $(this).find('.description').html("Cette description est à destination des malvoyants et moteurs de recherches. <br/> Si possible ré-utiliser le <i>titre de la balade</i> ainsi que les mots clefs : <i>nature, balade, rando, radonnées, faune, flore</i>");
-		    	if(index == 1) $(this).find('.description').html("Si des crédits sont associés à la photo, veuillez l'ajouter entre parenthèses. <br/> Pas de majuscule ou accent <br/> Préferer le séparateur '-' entre les expressions <br/> Si possible ré-utiliser le <i>titre de la balade</i> ainsi que les mots clefs : <i>nature, balade, rando, radonnées, faune, flore</i>");
-
-
+			    });
+			 
+			 
 		    });
 
-		    //Photos 
-		    $('#edit-field-image table.sticky-table tr').each(function(index){
+		}
 
-
-	    		$(this).find('.form-type-textfield').each(function(i){
-	    			if(i == 0) $(this).find('.description').html("Cette description est à destination des malvoyants et moteurs de recherches. <br/> Si possible ré-utiliser le <i>titre de la balade</i> ainsi que les mots clefs : <i>nature, balade, rando, radonnées, faune, flore</i>");
-	    			if(i == 1) $(this).find('.description').html("Si des crédits sont associés à la photo, veuillez l'ajouter entre parenthèses. <br/> Pas de majuscule ou accent <br/> Préferer le séparateur '-' entre les expressions <br/> Si possible ré-utiliser le <i>titre de la balade</i> ainsi que les mots clefs : <i>nature, balade, rando, radonnées, faune, flore</i>");
-	    		});
-	    		
-
-		    });
-
-
-		    
-		    // if ($(event.target.id) == 'edit-field-photo-resume-und-0-upload-button--2') {
-		      
-
-		    //   // Your code here
-		    //   console.log('toto');
-
-		    // }
-	    });
-
-
-
-	    //Upload Photo résume
-	    /*$('.field-name-field-photo-resume', context).change(function () {
-
-			$(this).find('.form-type-textfield').each(function(index){
-	      		
-	  			console.log('rsume : '+$(this).find('.description').text());
-
-
-	      	});
-
-	    });*/
-    
 
     } //Fin attach
   };
