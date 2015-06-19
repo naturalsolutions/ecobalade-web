0c315c4075429d63c65204b2090d9a435042ecd0
gitkyo <p.delaunay1@gmail.com>
2015-06-19 16:20:54 +0200

    Ajout script tag manager google

--- ecobaladeTwitter/templates/html.tpl.php
+++ ecobaladeTwitter/templates/html.tpl.php
@@ -47,6 +47,15 @@
   <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-52f11cb3439d368f" async="async"></script>
 
   <div id="fb-root"></div>
+  <!-- Google Tag Manager -->
+  <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-TJPB8G"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-TJPB8G');</script>
+  <!-- End Google Tag Manager -->
   <script>(function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
