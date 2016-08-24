f4afbe97a4a8ed72e6a102418cdccf7d97fd0bcd
gitkyo <p.delaunay1@gmail.com>
2016-08-24 17:04:12 +0200

    [#128749525] Part2 - Amélioration de la structures des tires Hn dans les templates

--- ecobaladeTwitter/templates/comment-wrapper.tpl.php
+++ ecobaladeTwitter/templates/comment-wrapper.tpl.php
@@ -1,7 +1,7 @@
 <section id="comments" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php if ($content['comments'] && $node->type != 'forum'): ?>
     <?php print render($title_prefix); ?>
-    <h2 class="title"><?php print t('Comments'); ?></h2>
+    
     <?php print render($title_suffix); ?>
   <?php endif; ?>
 
